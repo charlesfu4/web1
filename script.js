2950c75be13d87c3be9c909dc0c49a04d9dec547
Charles Fu <charlesfu4@gmail.com>
2015-10-06 00:01:33 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('#text').click(function(){
+        $(this).addClass('highlighted');
+        });
+    
+    });
+$(document).ready(function(){
+    $('#title').click(function(){
+        $(this).addClass('highlighted');
+        });
+    
+    });

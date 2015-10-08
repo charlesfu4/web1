8f7355875d7955f9bb99249adf0e5a189eccd7fc
Charles Fu <charlesfu4@gmail.com>
2015-10-08 11:27:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-$(document).ready(function(){
-    $('#text').click(function(){
-        $(this).toggleClass('highlighted');
-        });
+var main =function(){
+    $('.icon-menu').click(function(){
+        $('.menu').animate({left:'0px'},200);
+        $('body').animate({left:'285px'},850);
+        })
+    $('.icon-close').click(function(){
+        $('.menu').animate({left:'-285px'},850);
+        $('body').animate({left:'0px'},200);
+        })        
+        };
     
-    });
-$(document).ready(function(){
-    $('#title').click(function(){
-        $(this).toggleClass('highlighted');
-        });
-    
-    });
+
+
+$(document).ready(main);

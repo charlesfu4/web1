7071166856de07a243c8487f45bc0c725bd442b8
Charles Fu <charlesfu4@gmail.com>
2015-10-10 00:11:29 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,15 @@ var main =function(){
         $('.menu').animate({left:'-285px'},850);
         $('body').animate({left:'0px'},200);
         })        
-        };
+    $('.icon-menu').hover(
+        function(){
+        $(this).addClass('menu-color')},
+        function(){
+        $(this).removeClass('menu-color')    
+        });    
+    
+    
+};
     
 
 
